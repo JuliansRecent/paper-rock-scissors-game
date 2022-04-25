fcd49cff058bb528ee9e35dac28e8fe44fb34e04
JuliansRecent <juliansrecent@gmail.com>
2022-04-25 01:59:54 -0700

    Added all win and lose statements in game function
    Added for loop to run the game until a score of 5 is reached.
    Now there is a userScore, and computerScore.

--- game.js
+++ game.js
@@ -1,15 +1,54 @@
 //make a computer function that returns paper,rock, or scissors
-const myOptions = ["Rock", "Paper", "Scissors"];
 
 function computerPlay() {
    let computerPlay = Math.random();
    if (computerPlay < 0.33) {
-       computerPlay = "rock";
-   } else if (computerPlay >= 0.67) {
-       computerPlay = "paper"
+       computerSelection = 'Rock';
+   } else if (computerPlay <= 0.67) {
+       computerSelection = 'Paper'
    } else {
-       computerPlay = "scissors"
+        computerSelection = 'Scissors'
    }
-   return computerPlay
+   return computerSelection;
 }
-console.log(computerPlay)
+
+//write a function that plays a single round of Rock Paper Scissors. The function 
+//should take parameters, playerSelection and computerSelection. Then return a string 
+//that declares the winner of the round.
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == 'rock' && computerSelection == 'paper') {
+        return('You lose, paper beats rocks!');
+    } else if (playerSelection == 'paper' && computerSelection == 'scisssors') {
+        return('You lose, scissors beats paper!')
+    } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
+        return('You lose, rock beats paper!')
+    } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+        return('You win, rock beats scissors!') 
+    } else if (playerSelection == 'paper' && computerSelection == 'rock') {
+        return('You win, paper beats rock!')
+    } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+        return('You win, scissors beats paper!')
+    } else {
+        return('Its a tie');
+    }
+}
+//create player's and computer's score starting from 0 and increment their scores 
+//accordingly
+
+let playersScore = 0;
+let computersScore = 0;
+
+
+
+//create a fuction called game and call the playRound function inside of it to play a 
+//5 round game that keeps score and reports a winner of loser at the end.
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt("Rock, Paper, or Scissors?");
+        const computerSelection = computerPlay();
+        console.log(playRound(playerSelection, computerSelection));
+        console.log("Your score = " + playersScore);
+        console.log("Computer's score = " + computersScore)
+    }
+}
+
